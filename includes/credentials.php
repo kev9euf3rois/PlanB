640e54795d87d0f68f9e8942377d66a721e7a2ff
kev9euf3rois <118884361+kev9euf3rois@users.noreply.github.com>
2023-02-11 00:19:38 +0100

    update credential.php

--- includes/credentials.php
+++ includes/credentials.php
@@ -4,6 +4,6 @@ $DB_HOST = 'localhost'; // Host
 $DB_PORT = 5432; // Port
 $DB_NAME = 'PlanB'; // Name of the database
 $DB_USER = 'postgres'; // Username
-$DB_PASS = 'IchbinKevin03.'; // Password
+$DB_PASS = ''; // Password
 
 ?>
