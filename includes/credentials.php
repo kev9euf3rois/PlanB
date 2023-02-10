1fdbfa95891b91538de2dc00c2c31309eb6ce87c
Kev <email@example.comk.tamme2003@gmail.com>
2023-02-11 00:19:07 +0100

    update credentials.php

--- includes/credentials.php
+++ includes/credentials.php
@@ -3,7 +3,7 @@
 $DB_HOST = 'localhost'; // Host
 $DB_PORT = 5432; // Port
 $DB_NAME = 'PlanB'; // Name of the database
-$DB_USER = ''; // Username
-$DB_PASS = ''; // Password
+$DB_USER = 'postgres'; // Username
+$DB_PASS = 'IchbinKevin03.'; // Password
 
 ?>
