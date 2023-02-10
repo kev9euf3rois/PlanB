9e8ea8c2b08ccf53a836022b1d738c7e0398fe22
Kev <email@example.comk.tamme2003@gmail.com>
2023-02-11 00:16:02 +0100

    included navbar.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ session_start();
 include('./includes/connect.php');
 include('./includes/functions.php');
 
+$pageName = "Startseite";
 
 ?>
 <!DOCTYPE html>
@@ -13,9 +14,12 @@ include('./includes/functions.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <link href="./assets/css/bootstrap.css" rel="stylesheet">
+    <title>PlanB | <?php echo $pageName;?></title>
 </head>
 <body>
-    
+<div class="container">
+<?php include('./components/navbar.php');?>
+  </div>   
 </body>
 </html>
