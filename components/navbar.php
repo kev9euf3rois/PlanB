7ad9513580e596ad3106145a5461cff3b8b503cc
Kev <email@example.comk.tamme2003@gmail.com>
2023-02-11 00:16:26 +0100

    created navbar.php

---
+++ components/navbar.php
@@ -0,0 +1,14 @@
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <img src="./images/logo.png" style="height: 30px">
+        <span class="fs-4">&nbsp PlanB</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="index.php" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Tab 2</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Tab 3</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Tab 4</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Tab 5</a></li>
+      </ul>
+    </header>
